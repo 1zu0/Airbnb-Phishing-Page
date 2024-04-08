e9d85abcf9b54e2ff8d0399a6e5a4f9888aa3a6c
1zu0 <67436621+1zu0@users.noreply.github.com>
2024-04-08 04:27:22 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer>
+	 <div class="container pageFooter">
+			<div id="contentCurve"></div>
+			<p class="text-muted disclaimer text-center ng-binding" style="font-size: 11px">If you register a new account, you agree to Airbnb's <a href="https://www.airbnb.com/terms" target="_blank">Terms & Conditions</a> and <a href="https://www.airbnb.com/terms/privacy_policy" target="_blank">Privacy Policy</a>.</p>
+	 </div>
+</footer>
+</body>
+</html>
